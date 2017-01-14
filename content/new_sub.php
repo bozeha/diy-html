6fad35fc627650e4828df04873f21f19d6bd1b6f
BOZE BOZE <BOZE BOZE>
2017-01-15 00:12:13 +0200

    works ... few files upload

--- content/new_sub.php
+++ content/new_sub.php
@@ -67,24 +67,19 @@ if ($conn->query($sql) === TRUE) {
 */
 
 
-
-
-
-
-
-
-
-
-
 $target_dir = "../images/guides/".$upload_array ['guide_key']."/" ;
 mkdir($target_dir, 0777);
 echo $target_dir;
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
+foreach($_FILES['fileToUpload']['tmp_name'] as $key => $tmp_name)
+{
+    
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"][$key]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"][$key]);
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
@@ -99,7 +94,7 @@ if (file_exists($target_file)) {
     $uploadOk = 0;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 10000000) {
+if ($_FILES["fileToUpload"]["size"][$key] > 10000000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
@@ -114,11 +109,21 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$key], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"][$key]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
-}
+}    
+    
+    
+    
+    }
+
+
+
+
+/*
+*/
 $conn->close();
 ?>
--- new_sub.php
+++ new_sub.php
@@ -75,7 +75,19 @@ $connection->close();
 
   <div class="form-group">
     <label for="exampleInputFile">File input</label>
-     <input type="file" name="fileToUpload" id="fileToUpload">
+     <input type="file" name="fileToUpload[]" id="fileToUpload">
+    <small id="fileHelp" class="form-text text-muted">This is some placeholder block-level help text for the above input. It's a bit lighter and easily wraps to a new line.</small>
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputFile">File input</label>
+     <input type="file" name="fileToUpload[]" id="fileToUpload">
+    <small id="fileHelp" class="form-text text-muted">This is some placeholder block-level help text for the above input. It's a bit lighter and easily wraps to a new line.</small>
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputFile">File input</label>
+     <input type="file" name="fileToUpload[]" id="fileToUpload">
     <small id="fileHelp" class="form-text text-muted">This is some placeholder block-level help text for the above input. It's a bit lighter and easily wraps to a new line.</small>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
